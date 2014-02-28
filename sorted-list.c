8a64ff283b59c33150a19c6b269c86db4e87a088
Tobias Perelstein <tobias.perelstein@gmail.com>
2014-02-28 15:44:45 -0500

    SLInsertion working

--- sorted-list.c
+++ sorted-list.c
@@ -46,65 +46,91 @@ SortedListPtr SLCreate(CompareFuncT cf, DestructFuncT df)
 
 void SLDestroy(SortedListPtr list)
 {
+    if(!list){
+        return;
+    }
     /*Destroy all Nodes, data. Free on the nodes, and df(data)*/
     Node *ptr = NULL;
-    while(!(list->head == NULL)){
+    while(list->head != NULL){
         list->df(list->head->data);
         ptr = list->head;
         list->head = list->head->next;
         free(ptr);
     }
     free(list);
-
 }
+
 int SLInsert(SortedListPtr list, void *newObj)
 {
+    if(!list || !newObj){       //if there is no list, or new data, return successfully
+        return 0;               //there's just nothing to insert
+    }
+
+    //we definitely are inserting something now, so we create a new node
+    Node *newNode = createNode(newObj);
+
+    if(list->head == NULL || list->cf(list->head->data, newObj) < 0){
+        printf("Inserting\n");
+        newNode->next = list->head;
+        list->head = newNode;
+        return 1;
+    }
+
     Node *ptr = list->head;
     Node *prev= NULL;
 
-    if(list->head == NULL){
-        printf("Inserting\n");
-        Node *newNode = createNode(newObj);
-        list->head = newNode;
-    }
     while(ptr != NULL){
-        if(list->cf(ptr->data, newObj) == 0){
+        if(list->cf(ptr->data, newObj) == 0){ //no duplicates
             return 0;
         }
-        else if(list->cf(ptr->data, newObj) < 0){
+        else if(list->cf(ptr->data, newObj) < 0){ //if newObj is bigger, insert
             Node *newNode = createNode(newObj);
             printf("Inserting node\n");
             if(prev == NULL){       //the new object is bigger than the head
-                Node *temp = ptr;
-                list->head = newNode;
-                newNode->next = temp;
+                Node *temp = ptr;       //store head
+                list->head = newNode;   //change head
+                newNode->next = temp;   //set previous head to head->next
             }
-            else{
-                prev->next = newNode;
-                newNode->next = ptr;
-                newNode->refCount++;
-            }
-
-        }
-        else if(list->cf(ptr->data, newObj) >= 1){
-            Node *newNode = createNode(newObj);
-            printf("Inserting node\n");
-            Node *temp = ptr->next;
-            ptr->next = newNode;
-            newNode->next = temp;
+            prev->next = newNode;
+            newNode->next = ptr;
             newNode->refCount++;
+            return 1;
         }
-        else{
-            ;
+        else if(list->cf(ptr->data, newObj) > 0){ //if newObj is smaller, keeping going
+            prev = ptr;
+            ptr = ptr->next;
         }
-        prev = ptr;
-        ptr = ptr->next;
     }
+    prev->next = newNode;
     return 1;
 }
 
 int SLRemove(SortedListPtr list, void *newObj)
 {
+    if(!list || list->head == NULL || !newObj){
+        return 0;
+    }
+    if(list->cf(list->head->data, newObj) == 0){
+        Node *tmp = list->head;
+        list->head = list->head->next;
+        list->head->refCount++;     //Sortedlistptr is pointing to this node now
+        tmp->refCount--;            //Sortedlistptr no longer pointing to this node
+        if(tmp->refCount <= 0){    //if the node has nothing pointing to it (no iterator)
+            list->df(tmp->data);    //Destroys the data of the node
+            if(tmp->next != NULL){      //if temp has a next, decrement its refCount
+                tmp->next->refCount--;
+            }
+            free(tmp);
+            return 1;
+        }
+    }
+    Node *ptr = list->head;
+    Node *prev = NULL;
+    while(ptr != NULL){
+        if(list->cf(ptr->data, newObj) == 0){
+            Node *tmp = ptr;
+
+        }
 
 }
 
